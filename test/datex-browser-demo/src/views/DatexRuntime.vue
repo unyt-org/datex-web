8f28f7c5f3a916d44e37e9809d602022f37a706a
Benedikt Strehle <benedikt@unyt.org>
2026-02-21 22:52:26 +0100

    work on demo page

--- test/datex-browser-demo/src/views/DatexRuntime.vue
+++ test/datex-browser-demo/src/views/DatexRuntime.vue
@@ -4,15 +4,40 @@
       <h1 class="text-2xl font-bold mb-4">DATEX Demo</h1>
       <div>
         <ul>
-          <li>Endpoint: <code>{{ runtime.endpoint }}</code></li>
+          <li>Endpoint: <code class="select-all">{{ runtime.endpoint }}</code></li>
           <li>Version: <code>{{ runtime.version }}</code></li>
         </ul>
 
         <div class="mt-4">
-            <ul>
-              <li v-for="interf in comHubStatus.interfaces">
-                <b>{{ interf.properties.interface_type }}/{{ interf.properties.channel }}</b><span v-if="interf.properties.name"> ({{ interf.properties.name }})</span>
-                 - <code>{{ interf.properties.protocol }}</code>
+            <ul class="flex flex-col gap-2 w-2xl">
+              <li
+                  v-for="inter in comHubStatus.interfaces.toSorted((a: any, b: any) => a.properties.interface_type.localeCompare(b.properties.interface_type))"
+                  :key="inter.id"
+                  class="bg-gray-900 rounded-md p-2"
+              >
+                <div>
+                  <b>{{ inter.properties.interface_type }}/{{ inter.properties.channel }}</b><span v-if="inter.properties.name"> ({{ inter.properties.name }})</span>
+                </div>
+                <ul
+                    v-for="socket in inter.sockets.sort((a: any, b: any) => {
+                      if (a.properties.is_direct && !b.properties.is_direct) return -1;
+                      if (!a.properties.is_direct && b.properties.is_direct) return 1;
+                      return 0;
+                    })"
+                    class="ml-2">
+                  <li :key="socket.uuid" :title="socket.uuid">
+                    <div>
+                      <span class="mr-2">
+                        <span v-if="socket.direction == 'InOut'">◀─▶</span>
+                        <span v-else-if="socket.direction == 'In'">◀─</span>
+                        <span v-else-if="socket.direction == 'Out'">─▶</span>
+                      </span>
+                      <span class="text-endpoint select-all">{{ socket.endpoint }}</span>
+                      <span v-if="socket.properties.is_direct" class="bg-cyan-600 px-1.5 py-0.5 rounded-md ml-2">direct</span>
+                      <span v-if="socket.properties.distance !== undefined" class="bg-gray-600 px-1.5 py-0.5 rounded-md ml-2">distance: {{ socket.properties.distance }}</span>
+                    </div>
+                  </li>
+                </ul>
               </li>
             </ul>
         </div>
@@ -28,6 +53,7 @@ const comHubStatus = ref(runtime.comHub.getMetadata());
 setInterval(() => {
   comHubStatus.value = runtime.comHub.getMetadata();
 }, 500);
+console.log(comHubStatus.value)
 
 </script>
 
